3e9ab66d03b6d37153a09470d20915f5180f9297
plexhd153 <124733503+plexhd153@users.noreply.github.com>
2023-02-17 15:47:00 -0300

    Add files via upload

---
+++ jogos/tabuleiro123/offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1676659519,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprtabuleiro-sheet0.png",
+		"images/sprjogador-sheet0.png",
+		"images/sprdado-sheet0.png",
+		"images/sprdado-sheet1.png",
+		"images/sprdado-sheet2.png",
+		"images/sprcasa-sheet0.png",
+		"media/dice.ogg",
+		"media/mover.ogg",
+		"media/errado.ogg",
+		"loading-logo.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"meucss.css"
+	]
+}
